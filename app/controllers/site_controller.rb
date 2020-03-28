83ae0680f4349f75ccf58ab323de2b18ca2de804
Rasikh Mukaram <rasikh@Rasikhs-MacBook-Pro.local>
2020-03-28 17:52:51 +1100

    initial setup

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,10 +1,13 @@
 class SiteController < ApplicationController
   def index
+    @title = "Welcome to RailSpace!"
   end
 
   def about
+    @title = "About RailSpace"
   end
 
   def help
+    @title = "RailSpace Help"
   end
 end

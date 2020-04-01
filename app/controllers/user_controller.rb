d6bd90b954a43711f52c09feaf6b9030d53b3515
Rasikh Mukaram <rasikh@Rasikhs-MacBook-Pro.local>
2020-04-02 00:18:44 +1100

    working with register and errors

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,8 +1,17 @@
 class UserController < ApplicationController
   def index
+    @title = "RailSpace User Hub"
   end
 
   def register
     @title = "Register"
+
+    if request.post? and params[:user]
+      @user = User.new(params[:user])
+      if @user.save
+        flash[:notice] = "User <b>#{@user.screen_name}</b> created!"
+        redirect_to :action => "index"
+      end
+    end
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+    # Returns a link for use in layout navigation.
+    def nav_link(text, controller, action = "index")
+        link_to_unless_current text, :controller => controller, :action => action
+    end
 end
